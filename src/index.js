20168edf534932c7daed46f23c49a5fcd0d39394
Juliya Shamray <fizmatka908990@gmail.com>
2023-09-14 15:27:38 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import 'modern-normalize';
 import { GlobalStyle } from 'styles/GlobalStyle';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
